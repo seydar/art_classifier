1f4a69d9e3bd6b9471f35e1f1d50388b8f406bf1
Ari Brown <arizzle@caen-vnc06.engin.umich.edu>
2012-11-04 17:20:04 -0500

    db_setup and fake oop!

--- db_setup.m
+++ db_setup.m
@@ -1,4 +1,35 @@
-function db_setup
-  conn = database('foo','[your user name]','asdf','com.mysql.jdbc.Driver','jdbc:mysql://localhost:3306/foo');
+function db = db_setup(path)
 
+  function image = get_image(name)
+    full_path = [path '/images/' name '.jpg'];
+    img = imread(full_path);
+
+    listing = dir([path '/features/' name '.*']);
+    listing = arrayfun(@(x) x.name, listing);
+
+    features = struct();
+    for i = 1:size(listing, 2)
+      picture = regexp(listing(i), '\.([^.]+)$', 'match');
+      load(listing, 'data');
+      features.(picture) = data;
+    end
+
+    function save
+      fields = fieldnames(features);
+      for i = 1:length(fields)
+        save_path = [path '/features/' name '.' fields{i}];
+        data = features.(fields{i});
+        save(save_path, 'data');
+      end
+    end
+
+    image = struct();
+    image.image = img;
+    image.features = features;
+    image.save = @save;
+  end
+
+  db = struct();
+  db.get_image = @get_image;
+end
 
