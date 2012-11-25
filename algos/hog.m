1a5352397d9d1f2bcffad586a8fbf11901a154cb
Ari Brown <arizzle@caen-vnc04.engin.umich.edu>
2012-11-25 16:46:58 -0500

    our implementation of hog

--- algos/hog.m
+++ algos/hog.m
@@ -1,28 +1,57 @@
-function result = hog(pixels, params)
+% from http://www.mathworks.com/matlabcentral/fileexchange/28689-hog-descriptor-for-matlab
+function result = hog(pixels)
   pixels    = double(pixels);
   s         = size(pixels);
-  grayscale = 1;
 
-  if nargin < 2
-    params = [9, 8. 2, 0, 0.2];
+  nwin_x = 3;
+  nwin_y = 3;
+  nbins  = 9;
+  H      = zeros(nwin_x * nwin_y * nbins, 1);
+  m      = sqrt(s(1) / 2);
+
+  if s(1) == 1
+    pixels = im_recover(pixels, m, 2 * m);
+    s = [2 * m, m];
   end
 
-  hist1 = 2 + ceil(-0.5 + s(1) / params(2));
-  hist2 = 2 + ceil(-0.5 + s(2) / params(2));
+  step_x  = floor(s(2) / (nwin_x + 1));
+  step_y  = floor(s(1) / (nwin_y + 1));
+  cont    = 0;
+  hx      = [-1 0 1];
+  hy      = -hx;
 
-  result = [];
+  grad_xr = imfilter(pixels, hx);
+  grad_yu = imfilter(pixels, hy);
+  angles  = atan2(grad_yu, grad_xr);
+  mag     = ((grad_yu .^ 2) + (grad_xr .^ 2)) .^ 5;
 
-  nb_bins    = params(1);
-  cwidth     = params(2);
-  orient     = params(4);
-  block_size = params(3);
-  clip_val   = params(5);
+  for n = 0:(nwin_y - 1)
+    for m = 0:(nwin_x - 1)
+      cont    = cont + 1;
+      angles2 = angles((n * step_y + 1):((n + 2) * step_y),
+                       (m * step_x + 1):((m + 2) * step_x));
+      mag2    =    mag((n * step_y + 1):((n + 2) * step_y),
+                       (m * step_x + 1):((m + 2) * step_x));
 
-  bin_size   = (1 + (orient == 1)) * pi / nb_bins;
-  dx, dy = [];
+      v_angles = angles2(:);
+      v_mag    = mag2(:);
 
-  // calculate gradients
-  [dx, dy] = gradient(pixels);
-  
+      K   = max(size(v_angles));
+      bin = 0;
+      H2  = zeros(nbins, 1);
+      for ang_lim = (-pi + 2 * pi / B):(2 * pi / B):pi
+        bin = bin + 1;
+        for k = 1:K
+          if v_angles(k) < ang_lim
+            v_angles(k) = 100;
+            H2(bin) = H2(bin) + v_mag(k);
+          end
+        end
+      end
+
+      H2 = H2 / (norm(H2) + 0.01);
+      H(((cont - 1) * nbins + 1):(cont * nbins), 1) = H2;
+    end
+  end
 end
 
