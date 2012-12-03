edf925c2b6b0ec86545c826459d08ad4e3450398
Ari Brown <arizzle@caen-vnc08.engin.umich.edu>
2012-12-02 19:26:49 -0500

    parallelizing produce_features and implementing lbp

--- algos/lbp.m
+++ algos/lbp.m
@@ -1,4 +1,25 @@
-function lbp(im)
+function h = lbp(im)
+  im = rgb2gray(im);
   
+  patterns = zeros((size(im, 1) - 2) * (size(im, 2) - 2));
+  for i = 2:(size(im, 1) - 1)
+    for j = 2:(size(im, 2) - 1)
+      patterns(i * j + j) = pattern(i, j, im);
+    end
+  end
+  
+  h = hist(patterns, 256);
 end
 
+  function num = pattern(i, j, im)
+    p = im(i, j);
+    num = [im(i-1, j-1) > p
+           im(i,   j-1) > p
+           im(i+1, j-1) > p
+           im(i+1, j)   > p
+           im(i+1, j+1) > p
+           im(i,   j+1) > p
+           im(i-1, j+1) > p
+           im(i-1, j)   > p];
+    num = num' * [1 2 4 8 16 32 64 128]';
+  end
--- produce_features.m
+++ produce_features.m
@@ -1,4 +1,6 @@
 function produce_features(base)
+  matlabpool open;
+  
   run 'vlfeat/toolbox/v1_setup'
   db = db_setup(base);
 
@@ -9,11 +11,9 @@ function produce_features(base)
   for i = 1:size(list, 1)
     new{i} = list(i).name;
   end
-
-  images = cellfun(@(x) db.get_image(x), new, 'uni', false);
-
-  for i = 1:size(images, 2)
-    image = images{i};
+  
+  parfor i = 1:size(new, 2)
+    image = db.get_image(x);
     image.hog = hog(image.image);
     image.sift = sift(image.image);
     image.corners = corners(image.image, );
