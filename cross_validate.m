4240c1be353af9a4386e2117fadb1a0efc1915c0
s1syphus <prometheus672@gmail.com>
2012-12-16 16:30:49 -0500

    parfor'd cross_validate -Mike

--- cross_validate.m
+++ cross_validate.m
@@ -1,4 +1,4 @@
-function [ total_first_rate total_second_rate ] = cross_validate( artist_1, artist_2, w, T )
+function [ tfr tsr ] = cross_validate( artist_1, artist_2, w, T )
 
     % 10-fold cross validation
 
@@ -11,23 +11,30 @@ function [ total_first_rate total_second_rate ] = cross_validate( artist_1, arti
     all_f_indices = 1:f_size;
     all_s_indices = 1:s_size;
     
-    total_first_rate = 0;
-    total_second_rate = 0;
+    total_first_rate = zeros(10,1);
+    total_second_rate = zeros(10,1);
     
-    for i=0:9
-        
+    parfor i=0:9
+       
+       disp(['testing i = ' num2str(i)])
        f_test = all_f_indices(mod(all_f_indices, 10) == i);
        f_train = all_f_indices(mod(all_f_indices, 10) ~= i);
        
        s_test = all_s_indices(mod(all_s_indices, 10) == i);
        s_train = all_s_indices(mod(all_s_indices, 10) ~= i);
        
-       [art_1_rate art_2_rate] = weight_all_feats(artist_1, artist_2, w, f_test, f_train, s_test, s_train, T)
+       [art_1_rate art_2_rate] = weight_all_feats(artist_1, artist_2, w, f_test, f_train, s_test, s_train, T);
            
-        total_first_rate = total_first_rate + (art_1_rate*(size(f_test)/f_size));
-        total_second_rate = total_second_rate + (art_2_rate*(size(s_test)/s_size));        
+       % total_first_rate = total_first_rate + (art_1_rate*(size(f_test)/f_size));
+       % total_second_rate = total_second_rate + (art_2_rate*(size(s_test)/s_size));
+      
+       total_first_rate(i) = art_1_rate*(size(f_test)/f_size);
+       total_second_rate(i) = art_2_rate*(size(s_test)/s_size);
        
     end
 
+    tfr = sum(total_first_rate);
+    tsr = sum(total_second_rate);
+    
 end
 
