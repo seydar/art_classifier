72b137e8275e5278b744a39e356900ce251a8fad
Mike <prometheus672@gmail.com>
2012-12-18 14:19:05 -0500

    working sift_test.m but not incorporated into weight_all_feats/cross validation -mike

--- sift_test.m
+++ sift_test.m
@@ -16,9 +16,8 @@ function [ tfr tsr ] = sift_test( artist_1, artist_2, level)
     total_first_rate = zeros(10,1);
     total_second_rate = zeros(10,1);
     
-	%parfor i=1:10
-      	  
-  %     disp(['testing i = ' num2str(i)])
+	parfor i=1:10
+       disp(['testing i = ' num2str(i)])
        f_test = all_f_indices(mod(all_f_indices, 10) == (i-1));
        f_train = all_f_indices(mod(all_f_indices, 10) ~= (i-1));
        
@@ -33,7 +32,7 @@ function [ tfr tsr ] = sift_test( artist_1, artist_2, level)
        total_first_rate(i) = art_1_rate*(size(f_test,2)/f_size(1));
        total_second_rate(i) = art_2_rate*(size(s_test,2)/s_size(1));
 	      
-%	end
+	end
 	
 	
     tfr = sum(total_first_rate);
@@ -73,47 +72,47 @@ function [fr sr] = svm_sift(db, level, art_1, art_2, f_test, f_train, s_test, s_
 		second_feats = [second_feats im2.features.(['rawsift' num2str(level)])];		
 	end
 	
-	[s_words, ~] = vl_ikmeans(second_feats, K);
+	[s_words, ~] = vl_ikmeans(second_feats, 10);
 
 	%generate codebook
 	
 	codebook = [f_words s_words];
-	
-	size(codebook)
+	codebook = single(codebook);
+%	size(codebook)
 	
 	%build kd-tree
 	
+	artist_tree = vl_kdtreebuild(codebook);
 	
 	%gen histograms for training data
-	
-	
-	
 	%gen histograms for testing data
-	
-	
-	%{
-	
+
+	[t_f_hist, t_s_hist] = gen_sift_hist_(db, artist_tree, codebook, level, firsts, seconds, f_train, s_train);
+	[f_test_hist, s_test_hist] = gen_sift_hist_(db, artist_tree, codebook, level, firsts, seconds, f_test, s_test);
+
 	
 	%training svm
 	
 	key = [repmat(0,length(f_train),1); repmat(1,length(s_train),1)];
 	
-	trained_svm = svmtrain([first_feats; second_feats], key);
+	trained_svm = svmtrain([t_f_hist; t_s_hist], key);
 	
 	
 	artist1_rate = 0;
 	artist2_rate = 0;
+
 	
-	for i=f_test(1:end)
-        im = db.get_image(firsts(i).name);
-        if (svmclassify(trained_svm, im.features.(['sifthist' art_1 art_2 num2str(level)])) == 0)
+	
+	for i=1:size(f_test_hist,1)
+    %    im = db.get_image(firsts(i).name);
+		if (svmclassify(trained_svm, f_test_hist(i,:)) == 0)
 			artist1_rate = artist1_rate + 1;
 		end
 	end
     
-    for i=s_test(1:end)
-        im = db.get_image(seconds(i).name);
-        if (svmclassify(trained_svm, im.features.(['sifthist' art_1 art_2 num2str(level)])) == 1)
+    for i=1:size(s_test_hist,1)
+   %     im = db.get_image(seconds(i).name);
+		if (svmclassify(trained_svm, s_test_hist(i,:)) == 1)
 			artist2_rate = artist2_rate + 1;			
 		end
     end
@@ -123,12 +122,49 @@ function [fr sr] = svm_sift(db, level, art_1, art_2, f_test, f_train, s_test, s_
     
 	fr = artist1_rate/size(f_test,2);
 	sr = artist2_rate/size(s_test,2);
-		%}
+	
 
 
 end
 
-%function [db] = gen_sift_hist_test(db, 
+function [first_hist, second_hist] = gen_sift_hist_(db, art_tree, code_book, level,  firsts, seconds, f, s)	%input is either training or testing
+
+	image_clusters = [];
+	all_hists = [];
+	first_hist = [];
+	second_hist = [];
+	
+	for j = f(1:end)
+		im = db.get_image(firsts(j).name);
+		d = im.features.(['rawsift' num2str(level)]);
+		image_clusters = [];
+		for k = 1:size(d,2)
+			[i, ~] = vl_kdtreequery(art_tree, code_book, single(d(:,k)));
+			image_clusters = [image_clusters; i];
+		end
+		hist_ = hist(double(image_clusters), 20);
+		hist_ = hist_ ./ sum(hist_);
+		first_hist = [first_hist; hist_];
+	end
+	
+	
+	for j = s(1:end)
+		im = db.get_image(seconds(j).name);
+		d = im.features.(['rawsift' num2str(level)]);
+		image_clusters = [];
+		for k = 1:size(d,2)
+			[i, ~] = vl_kdtreequery(art_tree, code_book, single(d(:,k)));
+			image_clusters = [image_clusters; i];
+		end
+		hist_ = hist(double(image_clusters), 20);
+		hist_ = hist_ ./ sum(hist_);
+		second_hist = [second_hist; hist_];
+	end
+
+	
+	
+	
+end
 
 
 
