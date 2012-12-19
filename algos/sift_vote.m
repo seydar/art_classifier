5d3227641514019d61e8e48c84d7550cafa52f92
unknown <pelops@CCS-AHA433.adsroot.itcs.umich.edu>
2012-12-18 22:05:31 -0500

    everything is working. time to get some data bitches -mike

--- algos/sift_vote.m
+++ algos/sift_vote.m
@@ -1,11 +1,11 @@
-function [vote] = sift_vote(sift_svms, image, artist_tree, codebook)
+function [vote] = sift_vote(sift_svms, image, artist_tree, codebooks)
 
 
 	vote_sum = 0;
 
 	for i=1:6
 	
-		hist_ = gen_one_sift_hist(artist_tree{i}, codebook, (i+2), image);
+		hist_ = gen_one_sift_hist(artist_tree{i}, codebooks{i}, (i+2), image);
 		if (svmclassify(sift_svms{i}, hist_) == 1)
 			vote_sum = vote_sum + 1;
 			if i == 6
--- weight_all_feats.m
+++ weight_all_feats.m
@@ -18,26 +18,29 @@ function [artist1_rate artist2_rate] = weight_all_feats(artist_1, artist_2, w, f
     for i = f_train(1:end) % for all of the training images
         im1 = db.get_image(firsts(i).name); % get the ith image
         im1_feats = {};
-        for j = 1:length(w-1)
+        for j = 1:(length(w)-1)
             im1_feats = [im1_feats im1.features.(feat_names{j})]; % make a ROW of the features for each
         end
         first_feats = [first_feats; im1_feats];
-	end
+    end
     
-	for i = s_train(1:end) % for all of the training images
+    for i = s_train(1:end) % for all of the training images
         im2 = db.get_image(seconds(i).name);
+%	disp(seconds(i).name)
         im2_feats = {};
-        for j = 1:length(w-1)
+        for j = 1:(length(w)-1)
             im2_feats = [im2_feats im2.features.(feat_names{j})];
         end
         second_feats = [second_feats; im2_feats];
-	end
+    end
     
+	disp('combined the feats going to generate the svms');
+	
     key = [repmat(0,length(f_train),1); repmat(1,length(s_train),1)];
     
     svms = {};
     
-	for i = 1:length(w-1)
+	for i = 1:(length(w)-1)
             ff = [];
             sf = [];
 		for j = 1:length(f_train)           
@@ -49,7 +52,7 @@ function [artist1_rate artist2_rate] = weight_all_feats(artist_1, artist_2, w, f
 		 svms = [svms svmtrain([ff; sf], key)];
 	end
 	
-	[sift_svms, art_trees, codebooks] = create_sift_svm(db, artist_1, artist_2, f_train, s_train);
+	[sift_svms, art_trees, codebooks] = create_sift_svm(artist_1, artist_2, f_train, s_train);
 	
 	disp('created sift_svms successfully');
 	
@@ -61,12 +64,12 @@ function [artist1_rate artist2_rate] = weight_all_feats(artist_1, artist_2, w, f
         im = db.get_image(firsts(i).name);
         wsum = 0;
 		for j=1:length(w)
-			if(feat_names{j} ~= 'sift')
+			if(strcmp(feat_names{j},'sift') == 0)
 				if (svmclassify(svms{j}, im.features.(feat_names{j})) == 0)
 					wsum = wsum + w(j);
 				end
 			else
-				if(sift_vote(sift_svms, im, art_trees,codebook) == 0)
+				if(sift_vote(sift_svms, im, art_trees,codebooks) == 0)
 					wsum = wsum + w(j);
 				end
 			end
@@ -82,12 +85,12 @@ function [artist1_rate artist2_rate] = weight_all_feats(artist_1, artist_2, w, f
         im = db.get_image(seconds(i).name);
         wsum = 0;
         for j=1:length(w)
-			if(feat_names{j} ~= 'sift')
+			if(strcmp(feat_names{j},'sift') == 0)
 				if (svmclassify(svms{j}, im.features.(feat_names{j})) == 1)
 					wsum = wsum + w(j);
 				end
 			else
-				if(sift_vote(sift_svms, im, art_trees,codebook) == 1)
+				if(sift_vote(sift_svms, im, art_trees,codebooks) == 1)
 					wsum = wsum + w(j);
 				end
 			end
@@ -100,8 +103,8 @@ function [artist1_rate artist2_rate] = weight_all_feats(artist_1, artist_2, w, f
         end
     end
 
- artist1_rate = sum(artist1_rate)/size(f_test,2);
- artist2_rate = sum(artist2_rate)/size(s_test,2);
+ artist1_rate = sum(artist1_rate)/size(f_test,2)
+ artist2_rate = sum(artist2_rate)/size(s_test,2)
  
  
  
